ed8af944570a6dc03c43156a7eb749f49953df11
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-02-22 16:08:47 +0000

    fix: adding the producer code

--- swapnilsrivastava/src/main/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilsrivastavaApplication.java
+++ swapnilsrivastava/src/main/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilsrivastavaApplication.java
@@ -2,6 +2,12 @@ package eu.swapnilsrivastava.swapnilsrivastava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
 public class SwapnilsrivastavaApplication {
@@ -10,4 +16,16 @@ public class SwapnilsrivastavaApplication {
 		SpringApplication.run(SwapnilsrivastavaApplication.class, args);
 	}
 
+}
+
+@RequiredArgsConstructor
+@Component
+class Producer {
+
+	private final KafkaTemplate<Integer, String> template;
+
+	@EventListener(ApplicationStartedEvent.class)
+	public void generate() {
+		template.send("hello_world", 1, "HelloWorld");
+	}
 }
