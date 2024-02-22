64e92cd7c8ad9c669e50a72e6ebf159e1aeb63a8
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-02-22 13:40:54 +0000

    fix: adding the gradle based spring apachae kafka project

---
+++ src/main/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilsrivastavaApplication.java
@@ -0,0 +1,13 @@
+package eu.swapnilsrivastava.swapnilsrivastava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapnilsrivastavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapnilsrivastavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilsrivastavaApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.swapnilsrivastava.swapnilsrivastava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapnilsrivastavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
