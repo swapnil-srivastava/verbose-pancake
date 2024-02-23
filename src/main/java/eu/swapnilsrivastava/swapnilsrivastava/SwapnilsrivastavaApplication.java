41ae3fe6881c8feb03503a8344a91a233bcefac5
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2024-02-23 14:26:50 +0000

    adding the consume changes and producer changes and code for creating the topic using topic builder

--- src/main/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilsrivastavaApplication.java
+++ src/main/java/eu/swapnilsrivastava/swapnilsrivastava/SwapnilsrivastavaApplication.java
@@ -1,10 +1,15 @@
 package eu.swapnilsrivastava.swapnilsrivastava;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +22,12 @@ public class SwapnilsrivastavaApplication {
 		SpringApplication.run(SwapnilsrivastavaApplication.class, args);
 	}
 
+	// Creating the topic using TopicBuilder which internally utlises KafkaAdmin API
+	// @Bean
+	// NewTopic helloSwapnil() {
+	// 	return TopicBuilder.name("hello-swapnil").partitions(12).replicas(13).build();
+	// }
+
 }
 
 @RequiredArgsConstructor
@@ -25,18 +36,20 @@ class Producer {
 
 	private final KafkaTemplate<Integer, String> template;
 
+	// Createing a data in the topic using KafkaTemplate which internally using Kafka Producer API
 	@EventListener(ApplicationStartedEvent.class)
 	public void generate() {
-		template.send("hello", 7, "swapnilsrivastava");
+		template.send("hello_world", 3, "swapnil-grey");
 	}
 }
 
 @Component
 class Consumer {
 
-	@KafkaListener(topics = {"hello"}, groupId = "springboot-swapnil")
-	public void consume(String message) {
-		System.out.println("LOADING ..........." + message);
+	// Reading the data using kafka listener annotation which internally using the Kafka Consumer API
+	@KafkaListener(topics = {"hello_world"}, groupId = "springboot-swapnil")
+	public void listen(ConsumerRecord<Integer, String> record) {
+		System.out.println("LOADING =========== " + "value:" + record.value() + " key:" + record.key());
 	}
 
 }
